82994bddedc3cf89a676e8b79157ade7795fb0c6
rogereufrasio <rogesilvarj@yahoo.com.br>
2026-07-26 17:24:31 -0300

    refactor: consolidate travel provider architecture

--- backend/src/infrastructure/providers/factory.py
+++
@@ -1,9 +0,0 @@
-from src.domain.travel.provider import TravelProvider
-from src.infrastructure.providers.mock_provider import MockTravelProvider
-
-
-class ProviderFactory:
-
-    @staticmethod
-    def create() -> TravelProvider:
-        return MockTravelProvider()
